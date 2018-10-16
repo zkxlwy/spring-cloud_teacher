4a769fd8e271c42fcc0c35922647ebcbdfa9632a
OrangeCat0517 <30237108+OrangeCat0517@users.noreply.github.com>
2018-10-16 16:21:44 +0800

    f

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -3,11 +3,14 @@ package com.example.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//git
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
+
     public static void main(String[] args) {
+
+        System.out.println();
         SpringApplication.run(EurekaApplication.class, args);
     }
 }
